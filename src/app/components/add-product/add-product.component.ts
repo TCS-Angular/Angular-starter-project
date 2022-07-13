ee75099310a2bab0d01e0f6df38f501c5082e715
ngactive99 <ngactive99@gmail.com>
2022-07-13 12:01:07 +0530

    validatoors added

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -9,17 +9,31 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AddProductComponent implements OnInit {
   addProductTypesForm : FormGroup;
 
+  contactForm = new FormGroup({
+    firstname: new FormControl(``,[Validators.required, Validators.minLength(5)]),
+    lastname: new FormControl(),
+    email: new FormControl() ,
+    gender: new FormControl() ,
+    isMarried : new FormControl() ,
+    country: new FormControl()
+  })
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+    
+  }
+
   constructor() { }
 
   ngOnInit(): void {
-    this.addProductTypesForm = new FormGroup({
-      "productName" : new FormControl(),
-      "productDescription" : new FormControl() 
-    })
+    // this.addProductTypesForm = new FormGroup({
+    //   "productName" : new FormControl(),
+    //   "productDescription" : new FormControl() 
+    // })
   }
-  addProductType() {
-    console.log(this.addProductTypesForm.value);
+  // addProductType() {
+  //   console.log(this.addProductTypesForm.value);
      
-  }
+  // }
 
 }
