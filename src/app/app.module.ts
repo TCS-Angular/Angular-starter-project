a76b2ece595f6732d572820d902ad3ef896e36be
ngactive99 <ngactive99@gmail.com>
2022-07-14 11:41:11 +0530

    created custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UserFormComponent } from './components/user-form/user-form.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { EllipsisPipe } from './ellipsis.pipe';
 
 
 
@@ -16,7 +17,8 @@ import { AddProductComponent } from './components/add-product/add-product.compon
     AppComponent,
     UserFormComponent,
     NavbarComponent,
-    AddProductComponent
+    AddProductComponent,
+    EllipsisPipe
   ],
   imports: [
     BrowserModule,
